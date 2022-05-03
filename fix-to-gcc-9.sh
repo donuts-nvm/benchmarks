d9a75e8809745c4d42978a0c7366186f5f50d43a
Kleber Kruger <kleberkruger@gmail.com>
2022-05-03 08:29:25 +0000

    Applied patch to fix source-code to gcc-9

--- fix-to-gcc-9.sh
+++ fix-to-gcc-9.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-for i in 0 1 2 3 4 5 6 7 8 9
-do
-    echo "Replacing '=item $i' to '=item C<$i>'"
-    grep -rl "=item $i" * | xargs sed -i "s/=item $i/=item C<$i>/g"
-done
+# for i in 0 1 2 3 4 5 6 7 8 9
+# do
+#     echo "Replacing '=item $i' to '=item C<$i>'"
+#     grep -rl "=item $i" * | xargs sed -i "s/=item $i/=item C<$i>/g"
+# done
 
-# use these commands below only once!
-grep -rl "HUGE" parsec/parsec-2.1/pkgs/apps/ferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
-grep -rl "HUGE" parsec/parsec-2.1/pkgs/netapps/netferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
+# # use these commands below only once!
+# grep -rl "HUGE" parsec/parsec-2.1/pkgs/apps/ferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
+# grep -rl "HUGE" parsec/parsec-2.1/pkgs/netapps/netferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
