7fb0920761f33dcfbfa45c830fa02f006defc5aa
Kleber Kruger <kleberkruger@gmail.com>
2022-05-03 09:55:19 +0000

    Updated lfs-git files list

--- git-lfs-track-files.sh
+++ git-lfs-track-files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+MAX_SIZE=50
 # assuming you are on your git repo folder and you generated a large file list (bigger than 50MB in this case), e.g.:
-find . -size +99M | cat > file-bigger-than-99MB.txt
+find . -size +${MAX_SIZE}M | cat > file-bigger-than-${MAX_SIZE}MB.txt
 
-flist="file-bigger-than-99MB.txt"
+flist="file-bigger-than-${MAX_SIZE}MB.txt"
 while IFS= read -r file
 do
   git lfs track "$file"
